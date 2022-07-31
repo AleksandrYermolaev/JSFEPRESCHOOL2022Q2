ce8fb4441a6b10a2171d3446dad4653a0266ca94
Aleksandr Yermolaev <sanyagt50@gmail.com>
2022-07-31 18:08:43 +0300

    feat: add weather widget

--- momentum/js/script.js
+++ momentum/js/script.js
@@ -42,21 +42,119 @@ function getTimeOfDay() {
 
 function setLocalStorage() {
 	localStorage.setItem('name', yourName.value);
+	localStorage.setItem('city', weatherCity.value);
 }
 
 function getLocalStorage() {
 	if (localStorage.getItem('name')) {
 		yourName.value = localStorage.getItem('name');
 	}
+	if (localStorage.getItem('city')) {
+		weatherCity.value = localStorage.getItem('city');
+		getWeather();
+	}
 }
 
-const greeting = document.querySelector('.greeting');
-const timeOfDay = getTimeOfDay();
-greeting.textContent = `Good ${timeOfDay}, `;
+function showGreeting() {
+	const greeting = document.querySelector('.greeting');
+	const timeOfDay = getTimeOfDay();
+	greeting.textContent = `Good ${timeOfDay}, `;
+	setTimeout(showGreeting, 1000);
+} showGreeting();
+
 const yourName = document.querySelector('.name');
 window.addEventListener('beforeunload', setLocalStorage);
 window.addEventListener('load', getLocalStorage);
 
+//Слайдер изображений
+function setBg() {
+	const linksTemlates = {
+		'morning': 'https://raw.githubusercontent.com/AleksandrYermolaev/tasks-assets/assets/images/morning/',
+		'day': 'https://raw.githubusercontent.com/AleksandrYermolaev/tasks-assets/assets/images/afternoon/',
+		'evening': 'https://raw.githubusercontent.com/AleksandrYermolaev/tasks-assets/assets/images/evening/',
+		'night': 'https://raw.githubusercontent.com/AleksandrYermolaev/tasks-assets/assets/images/night/',
+	};
+	const imageUrl = `${linksTemlates[getTimeOfDay()]}${String(randomImageNumber).padStart(2, '0')}.jpg`;
+	const img = new Image();
+	img.src = imageUrl;
+	img.addEventListener('load', () => {
+		body.style.backgroundImage = `url('${imageUrl}')`;
+	});
+}
+
+function getNextSlide() {
+	if (randomImageNumber < 20) {
+		randomImageNumber += 1;
+	} else randomImageNumber = 1;
+	setBg();
+}
+
+function getPrevSlide() {
+	if (randomImageNumber > 1) {
+		randomImageNumber -= 1;
+	} else randomImageNumber = 20;
+	setBg();
+}
+
+let randomImageNumber = Math.floor(Math.random() * 20 + 1);
+const body = document.querySelector('body');
+setBg();
+const nextSlide = document.querySelector('.slide-next');
+const prevSlide = document.querySelector('.slide-prev');
+nextSlide.addEventListener('click', getNextSlide);
+prevSlide.addEventListener('click', getPrevSlide);
+
+// Погода
+const weatherCity = document.querySelector('.city');
+weatherCity.value = 'Minsk';
+const weatherIcon = document.querySelector('.weather-icon');
+const weatherTemp = document.querySelector('.temperature');
+const weatherDesc = document.querySelector('.weather-description');
+const weatherWind = document.querySelector('.wind');
+const weatherHumi = document.querySelector('.humidity');
+const windDirections = {
+	'north': [337.5, 22.5],
+	'northeast': [22.5, 67.5],
+	'east': [67.5, 112.5],
+	'southeast': [112.5, 157.5],
+	'south': [157.5, 202.5],
+	'southwest': [202.5, 247.5],
+	'west': [247.5, 292.5],
+	'northwest': [292.5, 337.5]
+};
+let direction;
+weatherCity.addEventListener('change', () => {
+	getWeather();
+	weatherCity.blur();
+});
+
+async function getWeather() {
+	const url = `https://api.openweathermap.org/data/2.5/weather?q=${weatherCity.value}&lang=en&appid=e1fc18dd49c14287ed9de7cdafd21b6a&units=metric`;
+	const errorData = document.querySelector('.weather-error');
+	try {
+		const res = await fetch(url);
+		const weatherData = await res.json();
+		weatherIcon.className = 'weather-icon owf';
+		weatherIcon.classList.add(`owf-${weatherData.weather[0].id}`);
+		weatherTemp.textContent = `${Math.round(weatherData.main.temp)}℃,`;
+		weatherDesc.textContent = weatherData.weather[0].description;
+		for (let windDirection in windDirections) {
+			if (weatherData.wind.deg >= windDirections[windDirection][0] && weatherData.wind.deg < windDirections[windDirection][1]) {
+				direction = windDirection; 
+			}
+		}
+		weatherWind.textContent = `Wind: ${direction}, ${Math.round(weatherData.wind.speed)} m/s`;
+		weatherHumi.textContent = `Humidity: ${weatherData.main.humidity}%`
+		errorData.textContent = '';
+	} catch (error) {
+		errorData.textContent = 'An error has occurred! Check the spelling of the city and your internet connection!';
+		weatherIcon.textContent = '';
+		weatherTemp.textContent = '';
+		weatherDesc.textContent = '';
+		weatherWind.textContent = '';
+		weatherHumi.textContent = '';
+	}
+} getWeather();
 
 
- 
+
